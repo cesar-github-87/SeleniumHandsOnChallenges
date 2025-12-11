200b79ff000b2e40838c910613d44fec793a7049
Cesar Barragan <cesar.bh87@gmail.com>
2025-12-10 22:45:40 -0600

    updated SearchEnginePage.java: replaced @FindBy searchBox, using direct locator By.

--- src/test/java/pageClasses/SearchEnginePage.java
+++ src/test/java/pageClasses/SearchEnginePage.java
@@ -1,13 +1,20 @@
 package pageClasses;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SearchEnginePage {
     WebDriver driver;
 
+    private By searchBox = By.xpath("//input");
+    @FindBy (xpath="//div[contains(@class, 'flex-1')]//button") WebElement searchButton;
 
     //constructor
     public SearchEnginePage(WebDriver driver){
@@ -15,13 +22,19 @@ public class SearchEnginePage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy (xpath="//input") WebElement searchBox;
-    @FindBy (xpath="//div[contains(@class, 'flex-1')]//button") WebElement searchButton;
+
 
 
     //Functions
     public void fillSearchBar(String searchText){
-            searchBox.sendKeys(searchText);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // CORRECCIÓN: Usar ExpectedConditions para encontrar el elemento en tiempo real
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(searchBox));
+
+        // Limpiar y luego escribir
+        element.clear();
+        element.sendKeys(searchText);
     }
 
     public void clickSearchbutton(){
